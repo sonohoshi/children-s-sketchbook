7f51b145540f00cc5e52cbdaeb7e31cc379cf70f
Kim sm <dev.sonohoshi@gmail.com>
2024-04-12 18:59:35 +0900

    뭐가 잘 안되는데 일단 집가서 보기

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 async function wapper(...levels) {
     let domparser = new DOMParser();
 
-    const VERSION = "v0.2 by kimsm";
-    console.log("Running popn class script", VERSION, "\nPLZ wait a minute...");
+    console.log("popn data crawler running.\nPLZ wait a minute...");
 
     const PLAY_DATA_URL = "https://p.eagate.573.jp/game/popn/unilab/playdata";
     const MEDAL_IMAGE_URL =
@@ -28,9 +27,7 @@ async function wapper(...levels) {
             .filter((li) => li.firstElementChild.className.startsWith("col"))
             .map((li) => [
               li.children[3].textContent,
-              li.children[3].firstChild.src
-                .replace(`${MEDAL_IMAGE_URL}/meda_`, "")
-                .replace(".png", ""),
+              li.children[3].firstChild.src,
               li.firstElementChild.lastElementChild.textContent,
               li.firstElementChild.firstElementChild.textContent,
             ])
@@ -59,6 +56,30 @@ async function wapper(...levels) {
       .flat()
       .sort((a, b) => b.point - a.point);
     console.log({ s })
+    return s;
 }
 
-wapper(48, 49);
+async function render(){
+  document.innerHTML = `
+    <body><canvas id="myCanvas" width="100" height="100"></canvas></body>
+    `;
+
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+    var baseImage = new Image();
+    baseImage.src = 'https://image.dcinside.com/viewimage.php?id=&no=24b0d769e1d32ca73de987fa11d028319b15983a4fe17b29dcc6f7288b8246fae44e12ec30598b2b48f72e9a1b7e4fdc40d2df309c0aae079c14b8c5573ae8885272fe1cd42ebb36ab';
+    baseImage.onload = function() {
+        // 캔버스 크기를 이미지 크기에 맞게 조정
+        canvas.width = baseImage.width;
+        canvas.height = baseImage.height;
+
+        // 원본 이미지를 캔버스에 그립니다.
+        ctx.drawImage(baseImage, 0, 0);
+
+        // 스티커 이미지 추가
+        addStickers();
+    };
+    // 원본 이미지 경로
+}
+
+wapper(48, 49).then(_ => render());
