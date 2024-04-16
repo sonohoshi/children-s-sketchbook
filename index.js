9539b8ddde69abba0f65fe3e0ee9c765dc0340e7
Kim sm <dev.sonohoshi@gmail.com>
2024-04-17 00:59:57 +0900

    코드 대충 정리하고 rawcdn 쓰고

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-async function wapper(...levels) {
-    let domparser = new DOMParser();
+async function crawl(...levels) {
+  let domparser = new DOMParser();
 
-    console.log("popn data crawler running.\nPLZ wait a minute...");
+  console.log("popn data crawler running.\nPLZ wait a minute...");
 
-    const PLAY_DATA_URL = "https://p.eagate.573.jp/game/popn/unilab/playdata";
-    const MEDAL_IMAGE_URL =
-      "https://eacache.s.konaminet.jp/game/popn/unilab/images/p/common/medal";
-  
-    function resToText(res) {
-      return res.arrayBuffer().then((buffer) => {
-        if (res.headers.get("Content-Type").includes("UTF-8")) {
-          return new TextDecoder().decode(buffer);
-        } else {
-          return new TextDecoder("Shift_JIS").decode(buffer)
-        }
-      })
-    }
+  const PLAY_DATA_URL = "https://p.eagate.573.jp/game/popn/unilab/playdata";
+  const MEDAL_IMAGE_URL =
+    "https://eacache.s.konaminet.jp/game/popn/unilab/images/p/common/medal";
 
-    function whatever(url, level) {
-      return fetch(url)
-        .then(resToText)
-        .then((text) => domparser.parseFromString(text, "text/html"))
-        .then((doc) => doc.querySelectorAll("ul.mu_list_table > li"))
-        .then((lis) => {
-          return Array.from(lis)
-            .filter((li) => li.firstElementChild.className.startsWith("col"))
-            .map((li) => [
-              li.children[3].textContent,
-              li.children[3].firstChild.src,
-              li.firstElementChild.firstElementChild.textContent,
-            ])
-            .map(([score, medal, song]) => {
-              return {
-                song,
-                score,
-                medal,
-                level,
-              };
-            });
-        });
-    }
+  function resToText(res) {
+    return res.arrayBuffer().then((buffer) => {
+      if (res.headers.get("Content-Type").includes("UTF-8")) {
+        return new TextDecoder().decode(buffer);
+      } else {
+        return new TextDecoder("Shift_JIS").decode(buffer)
+      }
+    })
+  }
 
-    let arr = new Array();
-    for(let i = 0; i < 15; i++){
-      arr.push(...(levels.map(level => [i, level])));
-    }
-  
-    const promises = arr.map(([page, level]) =>
-      whatever(`${PLAY_DATA_URL}/mu_lv.html?page=${page}&level=${level}`, level)
-    );
+  function whatever(url, level) {
+    return fetch(url)
+      .then(resToText)
+      .then((text) => domparser.parseFromString(text, "text/html"))
+      .then((doc) => doc.querySelectorAll("ul.mu_list_table > li"))
+      .then((lis) => {
+        return Array.from(lis)
+          .filter((li) => li.firstElementChild.className.startsWith("col"))
+          .map((li) => [
+            li.children[3].textContent,
+            li.children[3].firstChild.src,
+            li.firstElementChild.firstElementChild.textContent,
+          ])
+          .map(([score, medal, song]) => {
+            return {
+              song,
+              score,
+              medal,
+              level,
+            };
+          });
+      });
+  }
 
-    const s = (await Promise.all(promises))
-      .flat()
-      .sort((a, b) => b.point - a.point);
-    console.log({ s })
-    return s;
+  let arr = new Array();
+  for (let i = 0; i < 15; i++) {
+    arr.push(...(levels.map(level => [i, level])));
+  }
+
+  const promises = arr.map(([page, level]) =>
+    whatever(`${PLAY_DATA_URL}/mu_lv.html?page=${page}&level=${level}`, level)
+  );
+
+  const s = (await Promise.all(promises))
+    .flat()
+    .sort((a, b) => b.point - a.point);
+  console.log({ s })
+  return s;
 }
 
-async function render49(playData){
+async function render49(playData) {
   function logMapElements(value, key, map) {
     console.log(`m[${key}] = ${value.x}, ${value.y}`);
   }
@@ -146,7 +146,7 @@ IX,684,2278
   const m = new Map();
   newCsv.split('\n').map(line => {
     var song = line.split(',');
-      m.set(song[0], {x: song[1], y: song[2]})
+    m.set(song[0], { x: song[1], y: song[2] })
   });
   m.forEach(logMapElements);
 
@@ -155,7 +155,7 @@ IX,684,2278
    <style scoped>
   canvas {
     background-repeat: no-repeat;
-    background: url("https://private-user-images.githubusercontent.com/48484989/322400544-b827c324-d247-4aa0-bce0-78181d7cf310.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMyNzAyNjYsIm5iZiI6MTcxMzI2OTk2NiwicGF0aCI6Ii80ODQ4NDk4OS8zMjI0MDA1NDQtYjgyN2MzMjQtZDI0Ny00YWEwLWJjZTAtNzgxODFkN2NmMzEwLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDE2VDEyMTkyNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWEyNjA2NDJiNGM5OGY1YmQ3OTUxOTU4ZWIxZjJlYTNkZDA3MmUyNjUyMWJjZjE3ODY2MjI2ZDZiN2E2MDkzZjImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.gRJR964IJOJJt9_cAF6vpAIokRM7ds9I4-PBZ5K5E2o");
+    background: url("https://rawcdn.githack.com/sonohoshi/children-s-sketchbook/96906144d80d0dc758520853a421eee62d10777b/img/49table.png");
   }
   </style>
    <canvas id="canvas" width="1500" height="2381">
@@ -168,11 +168,11 @@ IX,684,2278
     var ctx = canvas.getContext("2d");
     var xy = m.get(element.song);
     var img = new Image();
-    img.onload = function(){
+    img.onload = function () {
       ctx.drawImage(img, xy.x, xy.y, 40, 40)
     }
     img.src = element.medal;
   });
 }
 
-wapper(49).then(data => render49(data));
+crawl(49).then(data => render49(data));
