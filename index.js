3f6cc937f57d1599c28fede0c1afc2500aa43d12
Kim sm <dev.sonohoshi@gmail.com>
2024-04-16 22:10:16 +0900

    대충 메달을 출력하는거까지 성공했음

--- index.js
+++ index.js
@@ -59,14 +59,28 @@ async function wapper(...levels) {
     return s;
 }
 
-async function render(){
+async function render49(playData){
+  console.log(playData)
   document.body.innerHTML = `
-    <body>
-    <img
-  class="fit-picture"
-  src="https://github-production-user-asset-6210df.s3.amazonaws.com/48484989/322400510-63f83a1e-56a4-4f13-8d2d-b9674e0accfc.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240415%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240415T080421Z&X-Amz-Expires=300&X-Amz-Signature=87af1d19e1b9eab4bee0138f1f9682dabdac76fc90d7c5ee7fcd6d32d7e793bb&X-Amz-SignedHeaders=host&actor_id=48484989&key_id=0&repo_id=785611895"/>
-    </body>
+   <body>
+   <style scoped>
+  canvas {
+    background-repeat: no-repeat;
+    background: url("https://private-user-images.githubusercontent.com/48484989/322400544-b827c324-d247-4aa0-bce0-78181d7cf310.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMyNzAyNjYsIm5iZiI6MTcxMzI2OTk2NiwicGF0aCI6Ii80ODQ4NDk4OS8zMjI0MDA1NDQtYjgyN2MzMjQtZDI0Ny00YWEwLWJjZTAtNzgxODFkN2NmMzEwLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDE2VDEyMTkyNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWEyNjA2NDJiNGM5OGY1YmQ3OTUxOTU4ZWIxZjJlYTNkZDA3MmUyNjUyMWJjZjE3ODY2MjI2ZDZiN2E2MDkzZjImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.gRJR964IJOJJt9_cAF6vpAIokRM7ds9I4-PBZ5K5E2o");
+  }
+  </style>
+   <canvas id="canvas" width="1500" height="2381">
+  </canvas>
+  </body>
     `;
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  var img = new Image();
+  img.onload = function(){
+    ctx.drawImage(img, 0, 0)
+  }
+  img.src = ("https://eacache.s.konaminet.jp/game/popn/unilab/images/p/common/medal/meda_none.png");
 }
 
-wapper(48, 49).then(_ => render());
+
+wapper(49).then(data => render49(data));
